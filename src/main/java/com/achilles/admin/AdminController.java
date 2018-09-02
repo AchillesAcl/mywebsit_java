990bae3642878380ea331e175dbdd0fbb7177751
Achilles <1035957559@qq.com>
2018-09-02 23:21:34 +0800

    将Spring Struts2 Hibernate改为Spring SpringMVC Hibernate

---
+++ src/main/java/com/achilles/admin/AdminController.java
@@ -0,0 +1,31 @@
+package com.achilles.admin;
+
+import com.achilles.service.AdminService;
+import org.springframework.core.annotation.AliasFor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
+
+/**
+ * @author Achilles 2018-09-01
+ */
+@Controller
+@RequestMapping("test")
+public class AdminController {
+    @Resource(name = "adminService")
+    private AdminService adminService;
+    @RequestMapping(value ="/save",method = RequestMethod.POST)
+    @ResponseBody
+    public Admin creatAdmin(){
+        Admin admin = new Admin();
+        admin.setId(1);
+        admin.setName("achilles");
+        admin.setPassword("123");
+        adminService.save(admin);
+        return admin;
+    }
+}
